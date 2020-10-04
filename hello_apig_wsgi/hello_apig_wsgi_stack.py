f8acedf264fa459749ecc029253d0b7bed0fbf3a
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2020-10-04 16:36:52 -0600

    hmm filesystem

--- hello_apig_wsgi/hello_apig_wsgi_stack.py
+++ hello_apig_wsgi/hello_apig_wsgi_stack.py
@@ -1,9 +1,40 @@
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_apigateway as apigw,
+    aws_lambda_python as lmb_py,
+    aws_ec2 as ec2,
+    aws_efs as efs,
+    aws_lambda as lmb,
+)
 
 
 class HelloApigWsgiStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+
+        vpc = ec2.Vpc(self, "vpc")
+
+        file_system = efs.FileSystem(self, "filesystem", vpc=vpc)
+
+        access_point = file_system.add_access_point(
+            "access-point", posix_user=efs.PosixUser(uid="0", gid="0")
+        )
+
+        mount_path = "/mnt/openapi"
+
+        function = lmb_py.PythonFunction(
+            self,
+            "function",
+            entry="./lambdas",
+            vpc=vpc,
+            filesystem=lmb.FileSystem.from_efs_access_point(access_point, mount_path),
+            environment={"mount_path": mount_path},
+        )
+
+        api = apigw.LambdaRestApi(
+            self,
+            "api",
+            handler=function,
+        )
--- setup.py
+++ setup.py
@@ -8,38 +8,33 @@ with open("README.md") as fp:
 setuptools.setup(
     name="hello_apig_wsgi",
     version="0.0.1",
-
     description="An empty CDK Python app",
     long_description=long_description,
     long_description_content_type="text/markdown",
-
     author="author",
-
     package_dir={"": "hello_apig_wsgi"},
     packages=setuptools.find_packages(where="hello_apig_wsgi"),
-
     install_requires=[
-        "aws-cdk.core==1.66.0",
+        "aws-cdk.core",
+        "aws-cdk.aws_lambda_python",
+        "aws-cdk.aws_apigateway",
+        "aws-cdk.aws_efs",
+        "mangum",
+        "fastapi",
+        "uvicorn",
     ],
-
     python_requires=">=3.6",
-
     classifiers=[
         "Development Status :: 4 - Beta",
-
         "Intended Audience :: Developers",
-
         "License :: OSI Approved :: Apache Software License",
-
         "Programming Language :: JavaScript",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-
         "Topic :: Software Development :: Code Generators",
         "Topic :: Utilities",
-
         "Typing :: Typed",
     ],
 )
