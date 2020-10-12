d9dc73023cb3dc9699410bce71d1983cb974ee48
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2020-10-12 15:28:03 -0600

    updates

--- app.py
+++ app.py
@@ -2,10 +2,30 @@
 
 from aws_cdk import core
 
-from hello_apig_wsgi.hello_apig_wsgi_stack import HelloApigWsgiStack
+from hello_apig_wsgi.pipeline_stack import PipelineStack
+from pydantic import BaseSettings
 
+
+class Config(BaseSettings):
+    """https://pydantic-docs.helpmanual.io/usage/settings/"""
+
+    account: str = "385504394431"
+
+    region: str = "us-east-2"
+
+    gh_username: str = "knowsuchagency"
+
+    gh_repo: str = "cdk-hello-apigw-asgi"
+
+
+config = Config()
 
 app = core.App()
-HelloApigWsgiStack(app, "hello-apig-wsgi")
+PipelineStack(
+    app,
+    "hello-apig-wsgi-pipeline",
+    config,
+    env={"account": config.account, "region": config.region},
+)
 
 app.synth()
---
+++ hello_apig_wsgi/pipeline_stack.py
@@ -0,0 +1,88 @@
+from aws_cdk import core
+from aws_cdk import aws_codepipeline as codepipeline
+from aws_cdk import aws_codepipeline_actions as cpactions
+from aws_cdk import pipelines
+
+from .hello_apig_wsgi_stack import HelloApigWsgiStack
+
+from pydantic import BaseSettings
+
+
+class WebServiceStage(core.Stage):
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        service = HelloApigWsgiStack(self, "WebService")
+
+        # self.url_output = service.url_output
+
+
+class PipelineStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, config: BaseSettings, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        source_action = cpactions.GitHubSourceAction(
+            action_name="GitHub",
+            output=source_artifact,
+            oauth_token=core.SecretValue.ssm_secure("/github/token", "1"),
+            owner=config.gh_username,
+            repo=config.gh_repo,
+            trigger=cpactions.GitHubTrigger.POLL,
+        )
+
+        synth_action = pipelines.SimpleSynthAction(
+            source_artifact=source_artifact,
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            install_command="npm install -g aws-cdk && pip install -r requirements.txt",
+            # build_command="pytest unittests",
+            synth_command="cdk synth",
+        )
+
+        pipeline = pipelines.CdkPipeline(
+            self,
+            "Pipeline",
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            pipeline_name="WebinarPipeline",
+            source_action=source_action,
+            synth_action=synth_action,
+        )
+
+        pre_prod_app = WebServiceStage(
+            self,
+            "Pre-Prod",
+            env={
+                "account": config.account,
+                "region": config.region,
+            },
+        )
+
+        pre_prod_stage = pipeline.add_application_stage(pre_prod_app)
+
+        # pre_prod_stage.add_actions(
+        #     pipelines.ShellScriptAction(
+        #         action_name="Integ",
+        #         run_order=pre_prod_stage.next_sequential_run_order(),
+        #         additional_artifacts=[source_artifact],
+        #         commands=[
+        #             "pip install -r requirements.txt",
+        #             "pytest integtests",
+        #         ],
+        #         use_outputs={
+        #             "SERVICE_URL": pipeline.stack_output(pre_prod_app.url_output)
+        #         },
+        #     )
+        # )
+
+        pipeline.add_application_stage(
+            WebServiceStage(
+                self,
+                "Prod",
+                env={
+                    "account": config.account,
+                    "region": config.region,
+                },
+            )
+        )
